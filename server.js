b1a4f9828c0c6e78a08517616d81e824038e7d44
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-03-16 10:37:42 +0530

    test api route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
+import userRouter from './routes/user.route';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -15,3 +15,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log(`Server listening on port: `, 3000)
 });
+
+app.use('/api', userRouter);
