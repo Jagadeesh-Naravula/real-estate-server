d638856b1b8151c38c5b4e16361bec44ff4a8d65
Jagadeesh N <n.jagadeesh@simplyfi.tech>
2024-04-20 09:27:45 +0530

    update api

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -19,11 +19,30 @@ export const deleteListing = async(req, res, next)=>{
         }
 
         if(req.user.id !== listing.userRef){
-            return next(errorHandler(401, 'Yoc can delete your own listing!'));
+            return next(errorHandler(401, 'You can delete your own listing!'));
         }
 
         await Listing.findByIdAndDelete(req.params.id);
         return res.status(200).json('Listing has been deleted!');
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing = async(req, res, next)=>{
+    try {
+        const listing = await Listing.findById(req.params.id);
+
+        if(!listing){
+            return next(errorHandler(404, 'Listing not found!'));
+        }
+
+        if(req.user.id !== listing.userRef){
+            return next(errorHandler(401, 'You can update your own listing!'));
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new : true});
+        return res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
